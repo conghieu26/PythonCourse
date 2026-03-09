a2c5a5a0bf7aea32071ce1769bfdea75f18fd8ed
hieu <hieunc00@gmail.com>
2026-03-09 14:55:57 +0700

    Have finished Mad Libs game

---
+++ PythonProject/Project/madlibsGame.py
@@ -0,0 +1,16 @@
+# Madlibs game
+# Word game where you create a story
+# by filling in blanks with random words
+from tabnanny import verbose
+
+adjective1 = input("Enter your a adjective (decription): ")
+noun1 = input("Enter your a noun (person, place, thing): ")
+adjective2 = input("Enter your a adjective (decription): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter your a adjective (decription): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
+

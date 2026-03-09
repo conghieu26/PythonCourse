85d13d22371dc90ccb61c5263a9f69f18be4750b
hieu <hieunc00@gmail.com>
2026-03-09 14:35:09 +0700

    Ex1: 1 Rectangle Area Calc

---
+++ PythonProject/Ex1/main.py
@@ -0,0 +1,7 @@
+# Exercise 1 Rectangle Area Calc
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * length
+
+print(f"the area is: {area}cm")
